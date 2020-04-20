a5e7bb14574dcac2014a41820cd1e827c95a488b
lowdev <lowrey97x@hotmail.fr>
2020-04-20 21:29:05 +0200

    Add form for toy creation page.

--- ui/flutter/lib/screen/app/toycreation/ToyCreationPage.dart
+++ ui/flutter/lib/screen/app/toycreation/ToyCreationPage.dart
@@ -1,29 +1,124 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ToyCreationPage extends StatelessWidget {
+class ToyCreationPage extends StatefulWidget {
+
+  @override
+  ToyCreationPageState createState() => ToyCreationPageState();
+}
+
+class ToyCreationPageState extends State<ToyCreationPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.white30,
-      appBar: createBar(),
       body: createBody(context)
     );
   }
 
   AppBar createBar() {
     return new AppBar(
-      title: new Text(
-        "Create",
-        style: new TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87),
+      elevation: 0,
+      title: Center(
+          child: Container(
+              child: IconButton(
+                icon: Icon(Icons.camera_alt, semanticLabel: "Add"),
+                onPressed: () => {},
+              )
+          )
       ),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blue, Colors.yellow]))
+      )
     );
   }
 
   Widget createBody(BuildContext context) {
-    return new Text("test");
+    return  Form(
+        key: formKey,
+        child: Column (
+          children: <Widget>[
+            createUploadImage(),
+            createTitleInput(),
+            createNumberOfPieceInput(),
+            createDescriptionInput()
+          ],
+        )
+    );
+  }
+
+  Widget createUploadImage() {
+    return GestureDetector(
+      onTap: () { print("Container was tapped"); },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(100.0),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.camera_alt),
+                Text("Ajouter une image")
+              ]
+            ),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              border: Border.all(width: 2.0, color: Colors.black)
+            )
+          )
+        ]
+      )
+    );
+  }
+
+  Widget createTitleInput() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Entrez le titre'
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return "A remplir";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget createNumberOfPieceInput() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Nombre de pieces'
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return "A remplir";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget createDescriptionInput() {
+    return TextFormField(
+      keyboardType: TextInputType.multiline,
+      decoration: InputDecoration(
+          labelText: 'Décrire'
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return "A remplir";
+        }
+        return null;
+      },
+    );
   }
 }
