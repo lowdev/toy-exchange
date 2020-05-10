fec0d651c94cec5cdab1cac9b5e648efab139e32
lowdev <lowrey97x@hotmail.fr>
2020-05-10 18:58:32 +0200

    Remove cors security from server side.

--- booter/springBoot/src/main/java/org/lowentropy/toyexchanging/WebConfig.java
+++ booter/springBoot/src/main/java/org/lowentropy/toyexchanging/WebConfig.java
@@ -2,6 +2,7 @@ package org.lowentropy.toyexchanging;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.lowentropy.toyexchange.toy.infrastructure.web"
 })
 public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/flutter/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
